15705b198a7f5718643dadcc147120faedb621d2
Makinde Iyanuoluwa <111610606+Nal-Tay@users.noreply.github.com>
2022-11-29 17:00:05 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    s = ""
-    for i in range(len(str)):
-        if i != n:
-            s = s + str[i]
-     return (s)
+    strtmp = ""
+    cont = 0
+    for c in str:
+        if cont == n:
+            pass
+        else:
+            strtmp += c
+        cont += 1
+    return strtmp
