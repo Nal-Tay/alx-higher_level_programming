9974bb43357a93aa54bc675fc6d7f84f598502e9
Makinde Iyanuoluwa <111610606+Nal-Tay@users.noreply.github.com>
2022-12-08 04:08:45 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string and type(roman_string) is str:
-        dictn = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-        value = 0
-        for a, x in zip(roman_string, roman_string[1:]):
-            if dictn[a] < dictn[x]:
-                value -= dictn[a]
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    p = 0
+
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if val[roman_string[c]] >= p:
+                res += val[roman_string[c]]
             else:
-                value += dictn[a]
-            value += dictn[roman_string[-1]]
-            return value
-        else:
-            return 0
+                res -= val[roman_string[c]]
+            p = val[roman_string[c]]
+    return res
