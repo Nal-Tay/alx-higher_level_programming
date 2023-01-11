882e6d7af2cb3c30219a95bbf53dfdbf017a6d72
Makinde Iyanuoluwa <111610606+Nal-Tay@users.noreply.github.com>
2023-01-11 05:18:25 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Reads from standard input and computes metrics.
+
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
@@ -9,6 +10,7 @@ prints the following statistics:
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
+
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
@@ -55,8 +57,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
-except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
