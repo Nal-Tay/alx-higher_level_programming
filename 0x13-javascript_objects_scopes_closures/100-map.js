c2b590ae1b59a477993eb12ad0a706572e8a0f75
Makinde Iyanuoluwa <111610606+Nal-Tay@users.noreply.github.com>
2023-03-15 06:03:31 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-
 const list = require('./100-data').list;
 
 console.log(list);
-console.log(list.map((x, i) => x * i));
+const newList = list.map(function (x, idx) {
+  return (x * idx);
+});
+console.log(newList);
